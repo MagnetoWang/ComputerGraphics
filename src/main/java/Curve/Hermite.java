930b0729cd78e6ec178a295c8d5846b0f7cc6efb
MagnetoWang <1527672216@qq.com>
2018-06-08 23:40:04 +0800

    hermite绘制完成。其实理解公式以后，画起来还是挺简单

--- src/main/java/Curve/Hermite.java
+++ src/main/java/Curve/Hermite.java
@@ -30,11 +30,11 @@ public class Hermite extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Formula one");
+		primaryStage.setTitle("Hermite");
 		root.getChildren().add(canvas);
 		primaryStage.setScene(new Scene(root));
 		
-		DrawHermite(200,primaryStage);
+		DrawHermite(500,primaryStage);
 
 
 
@@ -42,13 +42,32 @@ public class Hermite extends Application{
 		
 		
 	}
+	/***
+	 * hermite 有默认的公式直接调用即可。
+	 * @param points
+	 * @param primaryStage
+	 */
 	public void DrawHermite(int points,Stage primaryStage){
-		Point2D p1=new Point2D(0, 1);
-		Point2D p4=new Point2D(3,0);
-		Point2D r1=new Point2D(0, 1);
-		Point2D r4=new Point2D(3,0);
+		Point2D p1=new Point2D(100, 200);
+		Point2D p4=new Point2D(400,100);
+		Point2D r1=new Point2D(200, 100);
+		Point2D r4=new Point2D(-300,0);
 //		Point2D tempC0=new Point2D(x, y);
+		int n=points;
+		//要想画理想图，都是要对缩放特别设置
+		double scaler= 1.0/n;
+		for(int i=0;i<n;i++){
+			double u=i*scaler;
+			Point2D MixedPoint = p1.multiply(Math.pow(u, 3)*2-3*Math.pow(u, 2)+1)
+					.add(p4.multiply(Math.pow(u, 3)*(-2)+Math.pow(u, 2)*3)
+							.add(r1.multiply(u*u*u-2*u*u+u))
+							.add(r4.multiply(u*u*u-u*u)));
+			DrawPoint(MixedPoint.getX(), MixedPoint.getY(), 1	);
+		}
 		
+	}
+	public void DrawPoint(double x, double y, double radius) {
+		gc.fillOval(x, y, radius, radius);
 	}
 
 
