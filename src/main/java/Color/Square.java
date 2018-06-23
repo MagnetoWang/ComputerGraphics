998a35fca959e718f4f0cfca68c8d57530766fe9
MagnetoWang <1527672216@qq.com>
2018-06-24 00:04:27 +0800

    完成正方形画图。比较简单

--- src/main/java/Color/Square.java
+++ src/main/java/Color/Square.java
@@ -7,6 +7,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 /** 
 *
@@ -16,21 +18,62 @@ import javafx.stage.Stage;
 * @Description  显示方块的图形界面
 * 
 */
-public class Square {
+public class Square extends Application{
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 
 
     public Group root = new Group();
-	public Canvas canvas ;// new Canvas(800, 600);
+	public Canvas canvas ;//= new Canvas(800, 600);
 	GraphicsContext gc ;//= canvas.getGraphicsContext2D();
 	public Scene scene=new Scene(root);
 	
 	public void start(Stage primaryStage) throws Exception {
-
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+		canvas=new Canvas(n*10,n*10);
+		gc=canvas.getGraphicsContext2D();
 		root.getChildren().add(canvas);
+		
 		primaryStage.setScene(scene);
+		Rectangle rectangle=new Rectangle(n,n);
+		gc.setFill(Color.GRAY);
+		gc.fillRect(0, 0, n*10, n*10);
+		gc.setFill(Color.WHITE);
+		gc.fillRect(n*1, n*1, n, n);
+		
+		gc.setFill(Color.BLACK);
+		gc.fillRect(n*2+n, n*1, n, n);
+		
+		gc.setFill(Color.GREEN);
+		gc.fillRect(n*3+2*n, n*1, n, n);
+		
+		gc.setFill(Color.RED);
+		gc.fillRect(n*1, n*3, n, n);
+		
+		gc.setFill(Color.ORANGERED);
+		gc.fillRect(n*2+n, n*3, n, n);
+		
+		gc.setFill(Color.BLUE);
+		gc.fillRect(n*3+2*n, n*3, n, n);
+		
+		gc.setFill(Color.GOLD);
+		gc.fillRect(n*1, n*5, n, n);
+		
+		
+		gc.setFill(Color.PINK);
+		gc.fillRect(n*2+n, n*5, n, n);
+		
+		gc.setFill(Color.YELLOW);
+		gc.fillRect(n*3+2*n, n*5, n, n);
+		
+
+			
+		
+		
+		primaryStage.show();
+		
 		
 	}
 
